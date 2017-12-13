2902040dbed8e80a9d15ec2496c0ef844bece347
rathankalluri <ratan.kalluri@gmail.com>
2017-12-13 13:42:36 +0530

    Added Excel

--- dealer/views.py
+++ dealer/views.py
@@ -1,10 +1,28 @@
 from django.http import HttpResponse
 from dealer.models import autos
 from django.shortcuts import render
-from django.template import loader
+import pandas as pd
 
 # Create your views here.
 
 def index(requests):
 	output = autos.objects.all()
 	return render(requests, 'dealer/index.html', context={'output':output},)
+
+def excelgen(requests):
+	import StringIO
+
+	output = StringIO.StringIO()
+
+	df = pd.DataFrame(list(autos.objects.all().values('auto_id', 'auto_man', 'year','active','dealer_id')))
+	new_index = ['auto_id', 'auto_man', 'year','active','dealer_id']
+	df.reindex(new_index)
+
+	writer = pd.ExcelWriter('output.xlsx')
+	df.to_excel(writer, 'Sheet1')
+	writer.save()
+	excelbook = output.getvalue()
+	response = HttpResponse(content_type='application/vnd.ms-excel')
+	response['Content-Disposition'] = 'attachment; filename=Report.xlsx'
+	response.write(excelbook)
+	return response
