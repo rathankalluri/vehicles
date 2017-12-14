59217d924e4c06c5b10aa7bcc599cfb966febadc
rathankalluri <ratan.kalluri@gmail.com>
2017-12-14 13:39:21 +0530

    Update views.py

--- dealer/views.py
+++ dealer/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse
 from dealer.models import autos
 from django.shortcuts import render
 import pandas as pd
-
+from django.utils.encoding import smart_str
+import os
+import dealer
 # Create your views here.
 
 def index(requests):
@@ -10,19 +12,18 @@ def index(requests):
 	return render(requests, 'dealer/index.html', context={'output':output},)
 
 def excelgen(requests):
-	import StringIO
 
-	output = StringIO.StringIO()
-
+	pth = os.path.join(os.path.abspath(os.path.dirname(dealer.__file__)), "../")
 	df = pd.DataFrame(list(autos.objects.all().values('auto_id', 'auto_man', 'year','active','dealer_id')))
 	new_index = ['auto_id', 'auto_man', 'year','active','dealer_id']
 	df.reindex(new_index)
-
-	writer = pd.ExcelWriter('output.xlsx')
+	filename = 'report.xlsx'
+	writer = pd.ExcelWriter(filename)
 	df.to_excel(writer, 'Sheet1')
 	writer.save()
-	excelbook = output.getvalue()
+
 	response = HttpResponse(content_type='application/vnd.ms-excel')
-	response['Content-Disposition'] = 'attachment; filename=Report.xlsx'
-	response.write(excelbook)
+	#response['Content-Disposition'] = 'attachment; filename=Report.xlsx'
+	response['Content-Disposition'] = 'attachment; filename=%s' % smart_str(filename)
+	response['X-Sendfile'] = smart_str(pth)
 	return response
