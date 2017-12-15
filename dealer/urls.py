c48444f7014777cba9a0fc7c5e9333db0f0f06f3
rathankalluri <ratan.kalluri@gmail.com>
2017-12-15 13:33:49 +0530

    Update urls.py

--- dealer/urls.py
+++ dealer/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     # Home Page of App
     url(r'^$', views.index, name='index'),
     url(r'^excelgen/', views.excelgen, name='excelgen'),
+    url(r'^create/', views.create_new, name="create_new"),
+    url(r'^update', views.update_new, name="update_new"),
+    url(r'^delete', views.delete_new, name="delete_new"),
+    url(r'^test_form', views.test_form, name="test_form"),
 ]
